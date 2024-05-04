5ddaf2d61110b41e428c34fb7dcdae0beb6d52fb
easyflygit <akv888@inbox.ru>
2024-05-04 11:45:05 +0200

    не большая коррекция файла tasks.py для отображения всех чеков для данного метса, перед пробой переноса БД на PostgreSQL

--- analytics/tasks.py
+++ analytics/tasks.py
@@ -30,21 +30,21 @@ def calculate_analytics():
             place_of_purchase=place).aggregate(average_receipt=Avg('total_amount'))
 
         # Рассчитываем аналитику по категориям товаров для данного места
-        # for check in Check.objects.filter(place_of_purchase=place):
-        #     categories = check.category.all()
-        #     for category_instance in categories:
-        #         total_spent = Check.objects.filter(
-        #             place_of_purchase=place, category=category_instance).aggregate(total_spent=Sum('total_amount'))
-        #         average_receipt_category = Check.objects.filter(
-        #             place_of_purchase=place, category=category_instance).aggregate(average_receipt=Avg('total_amount'))
-        #
-        #         # Создаем или обновляем запись аналитики по категории товаров
-        #         category_analytics, created = CategoryAnalytics.objects.get_or_create(place=place, category=category_instance)
-        #         category_analytics.total_spent = total_spent['total_spent'] or 0
-        #         category_analytics.average_receipt = average_receipt_category['average_receipt'] or 0
-        #         category_analytics.save()
-        #     else:
-        #         print("No categories found for the check.")
+        for check in Check.objects.filter(place_of_purchase=place):
+            categories = check.category.all()
+            for category_instance in categories:
+                total_spent = Check.objects.filter(
+                    place_of_purchase=place, category=category_instance).aggregate(total_spent=Sum('total_amount'))
+                average_receipt_category = Check.objects.filter(
+                    place_of_purchase=place, category=category_instance).aggregate(average_receipt=Avg('total_amount'))
+
+                # Создаем или обновляем запись аналитики по категории товаров
+                category_analytics, created = CategoryAnalytics.objects.get_or_create(place=place, category=category_instance)
+                category_analytics.total_spent = total_spent['total_spent'] or 0
+                category_analytics.average_receipt = average_receipt_category['average_receipt'] or 0
+                category_analytics.save()
+            else:
+                print("No categories found for the check.")
 
         for category_instance in Category.objects.all():
             # Аннотируем каждый чек агрегированным значением total_amount для данной категории
